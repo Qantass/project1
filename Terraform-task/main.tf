5958c45f5e1b20c1e2cad8738ca7c1612fdb42f1
Qantass <82313520+Qantass@users.noreply.github.com>
2021-06-24 08:11:38 +0300

    Add subnets and NIs

--- Terraform-task/main.tf
+++ Terraform-task/main.tf
@@ -13,13 +13,63 @@ resource "aws_eip" "static_tom"{
   
 }
 
+#---------------------- V  P  C -------------------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "db_vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "tomcat-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.20.0/24"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "db-subnet"
+  }
+}
+
+resource "aws_network_interface" "tomcat" {
+  subnet_id   = aws_subnet.public_subnet.id
+  private_ips = ["10.0.10.10"]
+
+  tags = {
+    Name = "tomacat_internal_network_interface"
+  }
+}
+
+resource "aws_network_interface" "db" {
+  subnet_id   = aws_subnet.private_subnet.id
+  private_ips = ["10.0.20.10"]
+
+  tags = {
+    Name = "db_network_interface"
+  }
+}
+
+
+
+
 #------------------------------------------------------------------
 
 resource "aws_instance" "mysql" {
     ami = data.aws_ami.latest_ubuntu.id      # Linux Ubuntu Server 20.04 LTS 
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.db.id]
-    
+    network_interface {
+        network_interface_id = aws_network_interface.db.id
+        device_index         = 0
+    } 
     
   tags = {
     Name = "MySQL Server"
@@ -30,10 +80,15 @@ resource "aws_instance" "mysql" {
   }
 }
 
+
 resource "aws_instance" "tomcat" {
     ami = data.aws_ami.latest_ubuntu.id      # Linux Ubuntu Server 20.04 LTS
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.db.id]
+    network_interface {
+        network_interface_id = aws_network_interface.tomcat.id
+        device_index         = 0
+    }
     
   tags = {
     Name = "TomCat Server"
@@ -44,10 +99,3 @@ resource "aws_instance" "tomcat" {
   }
 }
 
-#---------------------- V  P  C -------------------------------
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "db_vpc"
-  }
-}
