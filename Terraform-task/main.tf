2c5e8c9dcc20ae96fcfaacedff002599bdabe11f
Qantass <82313520+Qantass@users.noreply.github.com>
2021-07-03 08:59:50 +0300

    Update main.tf

--- Terraform-task/main.tf
+++ Terraform-task/main.tf
@@ -3,7 +3,7 @@
 locals {
   ssh_user = "ubuntu"
   key_name = "taskkey"
-  private_key_path = "`/WorkLab/taskkey.pem"
+  private_key_path = "`/tmp/Taskkey.pem"
 }
 
 #----------------------- A  W  S ---------------------------
