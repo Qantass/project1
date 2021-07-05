0819972e053df502bcc3685e6fc3e1f54a552dd2
Qantass <rkiblyk@gmail.com>
2021-07-05 15:40:26 +0300

    Update set_ip.sh

--- Terraform-task/set_ip.sh
+++ Terraform-task/set_ip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i -r 's/.+mysql.+(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/'linux_MySQL ansible_host='\"$1/g hosts
-sed -i -r 's/.+tomcat.+(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/'linux_TomCAT ansible_host='\"$2/g hosts
+sed -i -r 's/.+mysql.+(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/'linux_MySQL ansible_host='\"$1/g hosts.txt
+sed -i -r 's/.+tomcat.+(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/'linux_TomCAT ansible_host='\"$2/g hosts.txt
